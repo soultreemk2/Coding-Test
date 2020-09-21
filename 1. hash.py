c6c7602bdb3b7a07e8e3c0058ec784c3906ea02f
양지현 <50647833+soultreemk2@users.noreply.github.com>
2020-09-21 16:53:34 +0900

    Update 1. hash.py

--- 1. hash.py
+++ 1. hash.py
@@ -55,6 +55,15 @@ def solution13(participant, completion):
 
 
 
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[len(participant)-1]
+
+
 
 
 
