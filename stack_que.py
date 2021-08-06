6d34eb95da5cc2bd8cb8142f6bd241e1d908e18e
양지현 <50647833+soultreemk2@users.noreply.github.com>
2021-08-06 13:54:36 +0900

    Update stack_que.py

--- stack_que.py
+++ stack_que.py
@@ -1,4 +1,4 @@
-###### while True: 문 와녑ㄱ 이해하기 ######
+###### while True: 문 완벽 이해하기 ######
 
 
 # 특정 input 값을 받기 전까지 계속해서 코드가 실행되게 하고 싶을 때 이용
@@ -24,3 +24,23 @@ while True:
 
     else:
         print('>> wrong answer!')
+
+	
+	
+# 프린트기 
+## 내가 짠 코드 - 테스트 케이스는 맞는데 범용성에서 실패
+
+def solution(priorities, location):
+    que = [(i,j) for i,j in enumerate(priorities)]
+    queque = [(i,j) for i,j in enumerate(priorities)]
+
+    while True:
+        aa = que.pop(0)
+
+        if aa[1] < max([que[i][1] for i in range(len(que))]):
+            que.append(aa)
+        else:
+            que.insert(0,aa)
+            break
+        
+    return(que.index(queque[location]) + 1)
