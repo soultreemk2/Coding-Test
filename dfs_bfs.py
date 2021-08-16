99ed9893b07a429eb5214f3dd0bfb29f7eac6bf2
양지현 <50647833+soultreemk2@users.noreply.github.com>
2021-08-17 01:36:09 +0900

    Update dfs_bfs.py

--- dfs_bfs.py
+++ dfs_bfs.py
@@ -6,31 +6,34 @@
 
 
 
+
+---------------------------------------------------------------------------------------------
+
 # 파알인- 전화번호 문자 조합
-## 매우 어렵다..
 
-def letterCombinations(self, digits:str) -> List[str]:
-  def dfs(index, path):
+digits = "235"
+dic = {"2":"abc", "3":"def", "4":"ghi","5":"jkl",
+       "6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+
+result = []
+
+def dfs(index, path):
     if len(path) == len(digits):
-      result.append(path)
-      return
-    
-    for i in range(index, len(digits)):
-      for j in dic[digits[i]]:
-        dfs(i+1, path+j)
-        
-   # 예외처리   
-   if not digits:
-    return []
-  
-  dic = {"2":"abc", "3":"def", "4":"ghi","5":"jkl",
-      "6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
-  
-  result = []
-  dfs(0, "")
-  
-  return result
+        result.append(path)
+        return
 
+    for i in range(index, len(digits)):
+        for j in dic[digits[i]]:
+            dfs(i+1, path+j)
+            
+    return result
+  
+  
+>>> dfs(0,"")  
+  
+  
+
+---------------------------------------------------------------------------------------------
 
 
 # 파알인- 조합의 합
@@ -83,10 +86,7 @@ def solution(candidates, target):
 >>> [[2, 2, 2, 2], [2, 3, 3], [3, 5]]
 
 
-
-
-
-
+---------------------------------------------------------------------------------------------
 
 
 # 파알인- 부분 집합
