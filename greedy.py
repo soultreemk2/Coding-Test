99c527cdcb1002109cb8fbbfed40ff6a12667498
양지현 <50647833+soultreemk2@users.noreply.github.com>
2021-08-20 00:58:52 +0900

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -211,7 +211,30 @@ def solution(name):
 ----------------------------------------------------------------------------------
 # 큰 수 만들기
 
-
+## 내 생각
+1. idx = list(range(0,len(nums))
+2. list(combination(idx,k))
+3. 2번에서 나온 인덱스 모음들을 nums에서 제외. --> append해서 max함수
+              
+# 정답 풀이
+def solution(number, k):
+    # stack에 입력값을 순서대로 삽입 
+    stack = [number[0]]
+    for num in number[1:]:
+        # 들어오는 값이 stack 값보다 크면, 기존의 값을 제거하고 새로운 값으로 바꿈 
+        # 참고 : len(stack) > 0 == stack
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            # 값을 한개 빼서 k는 1이 제거 
+            k -= 1
+            # 내부의 값을 제거 
+            stack.pop()
+        # 새로운 값을 삽입 
+        stack.append(num)
+    # 만일 충분히 제거하지 못했으면 남은 부분은 단순하게 삭제
+    # 이렇게 해도 되는 이유는 이미 큰 수부터 앞에서 채워넣었기 때문 
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
 
 
 
