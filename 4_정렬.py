d0752159fa5ba2fed232868802f2dc3ceec4b01f
양지현 <50647833+soultreemk2@users.noreply.github.com>
2021-09-01 10:57:54 +0900

    Update 4_정렬.py

--- 4_정렬.py
+++ 4_정렬.py
@@ -11,7 +11,7 @@ def solution(array, commands):
 ----------------------------------------------------------------------------------------------
   
 # 가장 큰 수
-## 다시 풀기
+## https://jokerldg.github.io/algorithm/2021/05/06/most-big-number.html
   
 def solution(numbers):
     numbers = list(map(str, numbers))
